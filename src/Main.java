0b87b25aeaae197e6340f78c1c4d3f4dfda40701
asmalouski <asmalaiks@gmail.com>
2015-05-31 13:45:04 +0300

    just first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by asmalouski on 31.5.15.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        Reader r  = new Reader();
+        r.scan();
+//        int k = 2;
+//        int huj = count(k);
+//        System.out.println("empty");
+    }
+
+    public static int count(int x){
+        x = x*2;
+        return x;
+    }
+}
---
+++ src/Reader.java
@@ -0,0 +1,11 @@
+/**
+ * Created by asmalouski on 31.5.15.
+ */
+public class Reader {
+    int i;
+    int k;
+
+    public void scan(){
+        System.out.println("Enter your first digit");
+    }
+}
